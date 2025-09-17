83ea3aed4a105cd4d08f932585cd0833784194c1
Batuhan Ardıç <batuhanardic50@gmail.com>
2025-09-18 01:00:13 +0300

    enablekafka

--- backend/src/main/java/com/biehcey/eventcart/eventcart/EventCartApplication.java
+++ backend/src/main/java/com/biehcey/eventcart/eventcart/EventCartApplication.java
@@ -3,9 +3,11 @@ package com.biehcey.eventcart.eventcart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableCaching
+@EnableKafka
 public class EventCartApplication {
 
 	public static void main(String[] args) {
