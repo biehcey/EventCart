e7bc10413f313cffbc146f68aa361fc0f7ebf545
Batuhan Ardıç <batuhanardic50@gmail.com>
2025-09-22 20:37:31 +0300

    product.owner ignored

--- backend/src/main/java/com/biehcey/eventcart/eventcart/cart/mapper/CartItemMapper.java
+++ backend/src/main/java/com/biehcey/eventcart/eventcart/cart/mapper/CartItemMapper.java
@@ -3,9 +3,13 @@ package com.biehcey.eventcart.eventcart.cart.mapper;
 import com.biehcey.eventcart.eventcart.cart.dto.CartItemDto;
 import com.biehcey.eventcart.eventcart.cart.entity.CartItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CartItemMapper {
+    @Mapping(target = "product.owner", ignore = true)
     CartItem toEntity(CartItemDto dto);
+
+    @Mapping(target = "product.owner", ignore = true)
     CartItemDto toDto(CartItem cartItem);
 }
