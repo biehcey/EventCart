6840ea214fa804b5fb4360f93ff8ac53bbfbee1e
Batuhan Ardıç <batuhanardic50@gmail.com>
2025-09-27 17:09:40 +0300

    createOrderFromCart method added

--- backend/src/main/java/com/biehcey/eventcart/eventcart/order/service/OrderService.java
+++ backend/src/main/java/com/biehcey/eventcart/eventcart/order/service/OrderService.java
@@ -2,7 +2,11 @@ package com.biehcey.eventcart.eventcart.order.service;
 
 import com.biehcey.eventcart.eventcart.authentication.entity.User;
 import com.biehcey.eventcart.eventcart.authentication.service.UserService;
+import com.biehcey.eventcart.eventcart.cart.entity.Cart;
 import com.biehcey.eventcart.eventcart.order.dto.OrderDto;
+import com.biehcey.eventcart.eventcart.order.entity.Order;
+import com.biehcey.eventcart.eventcart.order.entity.OrderItem;
+import com.biehcey.eventcart.eventcart.order.entity.OrderStatus;
 import com.biehcey.eventcart.eventcart.order.mapper.OrderMapper;
 import com.biehcey.eventcart.eventcart.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,5 +25,26 @@ public class OrderService {
         User user = userService.getCurrentUser();
         return orderRepository.findByCustomer(user)
                 .stream().map(orderMapper::toDto).toList();
+    }
+
+    public Order createOrderFromCart(User user, Cart cart){
+        Order order = new Order();
+        order.setCustomer(user);
+        order.setStatus(OrderStatus.CREATED);
+
+        List<OrderItem> orderItems = cart.getItems().stream().map(cartItem -> {
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            orderItem.setProductId(cartItem.getProduct().getId());
+            orderItem.setQuantity(cartItem.getQuantity());
+            orderItem.setPrice(cartItem.getProduct().getPrice());
+            orderItem.setQuantity(cartItem.getQuantity());
+            orderItem.calculateSubTotal();
+            return orderItem;
+        }).toList();
+        order.setItems(orderItems);
+        order.calculateTotalPrice();
+
+        return orderRepository.save(order);
     }
 }
