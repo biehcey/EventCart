b418824324b558f287ad2671116d60afbcd0b077
Batuhan Ardıç <batuhanardic50@gmail.com>
2025-09-27 16:13:39 +0300

    checkoutcontroller

---
+++ backend/src/main/java/com/biehcey/eventcart/eventcart/order/controller/CheckoutController.java
@@ -0,0 +1,32 @@
+package com.biehcey.eventcart.eventcart.order.controller;
+
+import com.biehcey.eventcart.eventcart.cart.repository.CartRepository;
+import com.biehcey.eventcart.eventcart.order.dto.CheckoutResponseDto;
+import com.biehcey.eventcart.eventcart.order.entity.Order;
+import com.biehcey.eventcart.eventcart.order.mapper.CheckoutMapper;
+import com.biehcey.eventcart.eventcart.order.service.CheckoutService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/checkout")
+@RequiredArgsConstructor
+public class CheckoutController {
+    private final CheckoutService checkoutService;
+    private final CartRepository cartRepository;
+    private final CheckoutMapper checkoutMapper;
+
+    @PostMapping("/{cartId}")
+    public ResponseEntity<CheckoutResponseDto> checkout(@PathVariable Long cartId){
+
+        Order order = checkoutService.checkout(cartId);
+
+        CheckoutResponseDto responseDto = checkoutMapper.toCheckoutResponse(order);
+        return ResponseEntity.ok(responseDto);
+    }
+
+}
